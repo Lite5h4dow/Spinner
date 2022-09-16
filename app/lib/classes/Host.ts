1f6d850017a34567e86161248bbd639dad43a359
Lamar Daughma <${{secrets.PERSONAL_EMAIL}}>
2022-09-16 21:06:23 +0100

    updating host class

--- app/lib/classes/Host.ts
+++ app/lib/classes/Host.ts
@@ -1,10 +1,35 @@
 import Docker from "dockerode";
-enum HostConnectionMethod{
-
+export enum HostConnectionMethod{
+  Default,
+  Socket,
+  Network
 }
 
-class Host{
-  docker:Docker
-  constructor(){
+export class Host{
+  connectionMethod:HostConnectionMethod;
+  name:string;
+  host?:string;
+  port?:number;
+  protocol?:"ssh"|"https"|"http";
+  socketPath?:string;
+
+  constructor(name:string, connectionMethod: HostConnectionMethod, config:{host?:string, protocol?:"ssh"|"https"|"http", port?:number, socketPath?:string}){
+    this.name = name;
+    this.connectionMethod = connectionMethod;
+    this.host = config.host;
+    this.protocol = config.protocol;
+    this.port = config.port;
+    this.socketPath = config.socketPath;
+  }
+
+  DockerConnector():Docker{
+    switch (this.connectionMethod) {
+      case HostConnectionMethod.Default:
+	return new Docker();
+      case HostConnectionMethod.Network:
+	return new Docker({protocol: this.protocol, host: this.host, port: this.port})
+      case HostConnectionMethod.Socket:
+	return new Docker({socketPath: this.socketPath});
+    }
   }
 }
