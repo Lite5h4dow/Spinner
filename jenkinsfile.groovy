99c3df27767f801c2f5660f2868cb8a65f4a1b56
Lamar Daughma <${{secrets.PERSONAL_EMAIL}}>
2022-09-24 17:26:37 +0100

    adding jenkinsfile

---
+++ jenkinsfile.groovy
@@ -0,0 +1,28 @@
+def spinnerImage
+pipeline:{
+  agent:any
+  options:{
+    timestamps()
+    timeout(time: 2, unit: 'HOURS')   // timeout on whole pipeline job
+  }
+
+  stages{
+    stage("Build Image"){
+      steps{
+	script{
+	  spinnerImage = docker.build("spinner")
+	}
+      }
+    }
+
+    stage("Push Image"){
+      steps{
+	script{
+	  docker.withRegistry("https://registry.litelot.us", "PrivateDockerCreds"){
+	    spinnerImage.push("latest")
+	  }
+	}
+      }
+    }
+  }
+}
